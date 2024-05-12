886ca8ddd57e18adc0b5a7e54093eadd7aa5e292
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-12 11:09:53 +0200

    fix: verify if eagle is flying before playing in sky

--- Eagle.java
+++ Eagle.java
@@ -24,35 +24,49 @@ public class Eagle extends Bird implements Fly{
 
     @Override
     public void takeOff() {
-        if(this.altitude > 0) {
+        if (this.isFlying()) {
             System.out.printf("%s is alredy in flight.%n", this.getName());
         } else {
             System.out.printf("%s takes off in the sky.%n", this.getName());
+            this.flying = true;
         }
     }
 
     @Override
     public void ascend(int altitude) {
-        this.altitude += altitude;
-        System.out.printf("%s flies upward, altitude: %s%n", this.getName(), this.altitude);
+        if (this.isFlying()) {
+            this.altitude += altitude;
+            System.out.printf("%s flies upward, altitude: %s%n", this.getName(), this.altitude);
+        } else {
+            System.out.printf("%s must take off before ascend", this.getName());
+        }
     }
 
     @Override
     public void descend(int altitude) {
-        this.altitude -= altitude;
-        if (this.altitude < 0) {
-            System.out.printf("%s is now a red smudge.%n", this.getName());
+        if (this.isFlying()) {
+            this.altitude -= altitude;
+            if (this.altitude < 0) {
+                System.out.printf("%s is now a red smudge.%n", this.getName());
+            } else {
+                System.out.printf("%s flies downward, altitude: %s%n", this.getName(), this.altitude);
+            }
         } else {
-            System.out.printf("%s flies downward, altitude: %s%n", this.getName(), this.altitude);
+            System.out.printf("%s must take off before descend", this.getName());
         }
     }
 
     @Override
     public void land() {
-        if(this.altitude > 1) {
-            System.out.printf("%s is too high, it can't land.%n", this.getName());
-        } else {
-            System.out.printf("%s lands on the ground.%n", this.getName());
+        if ( this.isFlying()) {
+            if (this.altitude > 1) {
+                System.out.printf("%s is too high, it can't land.%n", this.getName());
+            } else {
+                System.out.printf("%s lands on the ground.%n", this.getName());
+            }
+        }
+        else {
+            System.out.printf("%s must take off before land", this.getName());
         }
 
     }
